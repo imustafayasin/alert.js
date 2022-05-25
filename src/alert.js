35b7336b4c4625a059484ca6118357aeac27219a
imustafayasin <imustafayasin@gmail.com>
2022-05-26 00:33:50 +0300

    Refactoring

--- src/alert.js
+++ src/alert.js
@@ -13,45 +13,40 @@ class Alert {
 
     static setup() {
 
-        const alert_wrapper = this.elementCreator('div', 'alert-wrapper');
-        const alert_bg = this.elementCreator('div', 'alert-bg')
-        const alert_content = document.createElement("div");
+        const alert_wrapper = this.elementCreator({ type: 'div', _class: 'alert-wrapper', parentElement: document.body })
+        const alert_bg = this.elementCreator({ type: 'div', _class: 'alert-bg close_alert', parentElement: alert_wrapper })
+        const alert_content = this.elementCreator({ type: 'div', _class: 'alert-content', parentElement: alert_wrapper })
 
-        alert_content.className = "alert-content";
-        setTimeout(() => {
-            alert_wrapper.classList.add("show");
-        }, 250);
+        this.elementCreator({ type: 'h2', text: this._title, parentElement: alert_content })
 
-        const _title = document.createElement('h2')
-        _title.innerText = this._title;
-        alert_wrapper.appendChild(alert_bg)
-        alert_content.appendChild(_title)
+        const alert_actions = this.elementCreator({ type: 'div', _class: 'alert_actions', parentElement: alert_content })
+        const accept_button = this.elementCreator({ type: 'button', _class: 'accept_button close_alert', text: 'Okey', parentElement: alert_actions })
 
-        alert_wrapper.appendChild(alert_content)
-        document.body.appendChild(alert_wrapper)
-        alert_bg.addEventListener('click', (e) => {
-            alert_wrapper.classList.remove("show");
-            alert_wrapper.addEventListener('transitionend', () => {
-                alert_wrapper.remove()
-            })
-        })
+
+        this.show(alert_wrapper)
+        this.customEventListener('.close_alert', 'click', () => this.hide(alert_wrapper))
     }
 
+    static customEventListener(selector, listener, callback) {
+        document.querySelectorAll(selector).forEach(item => item.addEventListener(listener, () => { callback() }))
+    }
 
-    static elementCreator(type, _class, text, html, id, appendElement) {
-        let element = document.createElement(type);
-        element.className = _class;
-        element.innerText ??= text;
-        element.innerHTML ??= html;
-        element.id ??= id;
-        appendElement?.appendChild(element);
+    static elementCreator({ type, _class, text, html, id, parentElement }) {
+        let element = document.createElement(type)
+        element.className = _class ?? element.className
+        element.innerText = text ?? element.innerText
+        element.innerHTML = html ?? element.innerHTML
+        element.id = id ?? element.id
+        parentElement?.appendChild(element)
         return element
     }
-    show() {
-        this.hideAlert = true
+    static show(_element) {
+        setTimeout(() => _element.classList.add("show"), 250);
     }
-    hide() {
-        this.hideAlert = false
+    static hide(_element) {
+        console.log(_element)
+        _element.classList.remove("show")
+        _element.addEventListener('transitionend', () => _element.remove())
     }
 }
 
