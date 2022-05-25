4f2705c1e7aa0551c6340771132107b513b2ea71
imustafayasin <imustafayasin@gmail.com>
2022-05-25 23:35:53 +0300

    Object to class

--- src/alert.js
+++ src/alert.js
@@ -1,24 +1,29 @@
-const Swal = {
-    fire: function (title) {
+class Alert {
+
+    _title
+
+    static fire(title) {
         if (!!document.querySelector(".alert-wrapper")) return
         return new Promise((resolve, reject) => {
-            this.setup(title);
+            this._title = title;
+            this.setup();
             resolve()
         })
-    },
-    setup: function (title) {
-        const alert_wrapper = document.createElement('div');
-        const alert_bg = document.createElement('div');
+    }
+
+    static setup() {
+
+        const alert_wrapper = this.elementCreator('div', 'alert-wrapper');
+        const alert_bg = this.elementCreator('div', 'alert-bg')
         const alert_content = document.createElement("div");
 
-        alert_bg.className = "alert-bg";
-        alert_wrapper.className = "alert-wrapper";
         alert_content.className = "alert-content";
         setTimeout(() => {
             alert_wrapper.classList.add("show");
         }, 250);
+
         const _title = document.createElement('h2')
-        _title.innerText = title;
+        _title.innerText = this._title;
         alert_wrapper.appendChild(alert_bg)
         alert_content.appendChild(_title)
 
@@ -30,9 +35,27 @@ const Swal = {
                 alert_wrapper.remove()
             })
         })
-        alert
+    }
+
+
+    static elementCreator(type, _class, text, html, id, appendElement) {
+        let element = document.createElement(type);
+        element.className = _class;
+        element.innerText ??= text;
+        element.innerHTML ??= html;
+        element.id ??= id;
+        appendElement?.appendChild(element);
+        return element
+    }
+    show() {
+        this.hideAlert = true
+    }
+    hide() {
+        this.hideAlert = false
     }
 }
 
-export default Swal;
+
+
+export default Alert;
 
