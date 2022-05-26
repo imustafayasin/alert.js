7814c863817cac8932c6e9233eb746d94f9984e7
imustafayasin <imustafayasin@gmail.com>
2022-05-26 22:15:14 +0300

    Modal alert design

--- src/alert.js
+++ src/alert.js
@@ -1,11 +1,13 @@
 class Alert {
 
     _title
+    _description
 
-    static fire(title) {
+    static fire(title, description) {
         if (!!document.querySelector(".alert-wrapper")) return
         return new Promise((resolve, reject) => {
             this._title = title;
+            this._description = description;
             this.setup();
             resolve()
         })
@@ -14,17 +16,19 @@ class Alert {
     static setup() {
 
         const alert_wrapper = this.elementCreator({ type: 'div', _class: 'alert-wrapper', parentElement: document.body })
-        const alert_bg = this.elementCreator({ type: 'div', _class: 'alert-bg close_alert', parentElement: alert_wrapper })
+        const alert_bg = this.elementCreator({ type: 'div', _class: 'alert-bg close-alert', parentElement: alert_wrapper })
         const alert_content = this.elementCreator({ type: 'div', _class: 'alert-content', parentElement: alert_wrapper })
 
         this.elementCreator({ type: 'h2', text: this._title, parentElement: alert_content })
+        this.elementCreator({ type: 'p', text: this._description, parentElement: alert_content })
+        this.elementCreator({ type: 'button', _class:'close-modal-button close-alert', html: '&times;', parentElement: alert_content })
 
-        const alert_actions = this.elementCreator({ type: 'div', _class: 'alert_actions', parentElement: alert_content })
-        const accept_button = this.elementCreator({ type: 'button', _class: 'accept_button close_alert', text: 'Okey', parentElement: alert_actions })
+        const alert_actions = this.elementCreator({ type: 'div', _class: 'alert-actions', parentElement: alert_content })
+        const accept_button = this.elementCreator({ type: 'button', _class: 'accept-button close-alert', text: 'Okey', parentElement: alert_actions })
 
 
         this.show(alert_wrapper)
-        this.customEventListener('.close_alert', 'click', () => this.hide(alert_wrapper))
+        this.customEventListener('.close-alert', 'click', () => this.hide(alert_wrapper))
     }
 
     static customEventListener(selector, listener, callback) {
